981d13719fd7361febbd61ff9b810d53725bce2a
Komron Khisomov <normok_9595@bk.ru>
2024-06-24 17:05:48 +0500

    configurations

---
+++ src/nftoken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+
+import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/erc721.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract NFToken {
+    
+}
---
+++ test/nftoken.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {NFToken} from "../src/nftoken.sol";
+
+contract NFTokenTest is Test {
+    
+}
