2be155f28bfecaefa26e6c351db328c38b8d64e8
Komron Khisomov <normok_9595@bk.ru>
2024-06-25 13:47:23 +0500

    balanceOf and other settings

--- src/nftoken.sol
+++ src/nftoken.sol
@@ -6,5 +6,23 @@ import {ERC721} from "lib/openzeppelin-contracts/contracts/token/ERC721/erc721.s
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract NFToken {
-    
+    // ERRORS
+
+    // EVENTS
+
+    // STATE VARIABLES
+    mapping(address => uint256) private _balanceOf;
+
+
+    constructor() {
+
+    }
+
+    function balanceOf(address owner) public view returns (uint256){
+        return _balanceOf[owner];
+    }
+
+    function minting() public {}
+
+    function transfer() public {}
 }
